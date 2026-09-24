22367c945b3ac5482f09c6f438ed68cbd12d8195
Udaykalse <udaysinhk.2000@gmail.com>
2026-09-24 18:35:52 +0530

    feat: filter strings from array with and Without Filter Method

--- d3/app10.js
+++ d3/app10.js
@@ -1,4 +1,13 @@
 let arr = ["hello", 10, "world", 20, true, "js"];
 
 let str = arr.filter((s) => typeof s === "string");
-console.log(str)
+
+let str1 = [];
+for (let i = 0; i < arr.length; i++) {
+  if (typeof arr[i] === "string") {
+    str1.push(arr[i]);
+  }
+}
+
+console.log("With Filter Method:- ", str);
+console.log("Without Filter Method:- ", str);
