d7b0c4b7fb82371e6852fd8abd333befdc501ed4
Udaykalse <udaysinhk.2000@gmail.com>
2026-09-25 22:31:55 +0530

    feat: implement two sum on sorted array using two pointers

---
+++ d4/app1.js
@@ -0,0 +1,20 @@
+function twoSumSorted(numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    let sum = numbers[left] + numbers[right];
+    if (sum === target) {
+      return [left + 1, right + 1];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return [];
+}
+
+// Test
+console.log(twoSumSorted([2, 7, 11, 15], 9)); 
+console.log(twoSumSorted([2, 3, 4], 6));      
